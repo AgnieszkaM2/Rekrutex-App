598c9b8155a2b9ca8486ecefcb10b6e05752dbe8
Olusiowy <103048676+Olusiowy@users.noreply.github.com>
2022-04-07 13:53:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,10 +9,17 @@
     <body>
         <h1>REKRUTEX PHP</h1>
         
-        <?php
-        
+        <?php        
         print("PHP działa.");
-        
+        $baza=mysqli_connect("remotemysql.com","XtKYx97kfB","rt9JC3srMC","XtKYx97kfB");
+        if (mysqli_connect_errno())
+        {echo "Wystąpił błąd połączenia z bazą";}
+
+        $wynik = mysqli_query($baza,"SELECT * FROM pytania");
+
+        while($row = mysqli_fetch_array($wynik))
+        {echo $row['kategoria'] . " " . $row['tresc']; echo "<br>"; }
+        mysqli_close($baza);
         ?>
     </body>
     <script>
