c78933054900a9ced5efda3e491d033099d357d0
Olusiowy <103048676+Olusiowy@users.noreply.github.com>
2022-06-09 14:41:39 +0200

    Panel 3.5

--- api/dodaniePytaniaPanel.php
+++ api/dodaniePytaniaPanel.php
@@ -14,16 +14,16 @@ if($pytaniedata!=null){
     $odp2=$pytaniedata->odp2;
     $odp3=$pytaniedata->odp3;
     $odp4=$pytaniedata->odp4;
-    $poprawna=$pytaniedata->poprawna;
+    $poprawna=$pytaniedata->prawidlowa;
     $prawidlowa=$pytaniedata->prawidlowa;
 
-    $sql2 = "SELECT kategoria, tresc, login FROM pytania where (kategoria='$kategoria' and tresc ='$tresc')";
+    $sql2 = "SELECT kategoria, tresc FROM pytania where (kategoria='$kategoria' and tresc ='$tresc')";
     $res = mysqli_query($con,$sql2);
     $row = mysqli_fetch_array($res);
     if(is_array($row)){
       http_response_code(400);
     }else{
-      $sql = "INSERT INTO pytania(kategoria, tresc, odp1, odp2, odp3, odp4, poprawna, prawidlowa) VALUES('$kategoria', '$imie', '$odp1', '$odp2', '$odp3', '$odp4', '$poprawna', '$prawidlowa')";
+      $sql = "INSERT INTO pytania(kategoria, tresc, odp1, odp2, odp3, odp4, poprawna, prawidlowa) VALUES('$kategoria', '$tresc', '$odp1', '$odp2', '$odp3', '$odp4', '$poprawna', '$prawidlowa')";
       if($result = mysqli_query($con,$sql))
       {
           echo json_encode($result);
